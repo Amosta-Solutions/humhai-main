7b1569f2c2a6394d0501c58fe68cc3dbc0103a8c
Ankit0052 <135019338+Ankit0052@users.noreply.github.com>
2024-01-18 16:53:31 +0530

    update

--- src/Components/Website/Home.jsx
+++ src/Components/Website/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
+
+
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+  {/* <div class="container">
+    <div class="text-container">
+      <p class="static-text">Hi, I'm a</p>
+      <ul class="dynamic-text">
+        <li class="item">web developer</li>
+        <li class="item">mobile developer</li>
+        <li class="item">UX/UI designer</li>
+        <li class="item">content creator</li>
+        <li class="item">professor</li>
+      </ul>
+    </div>
+  </div> */}
+  </div>
   )
 }
 
