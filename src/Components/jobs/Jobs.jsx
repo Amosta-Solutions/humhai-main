bafc0fcd82f2d60dfbab602a9eb916d02fe38e48
Abdul Ansari <abdulansari251298@gmail.com>
2024-03-06 11:25:10 +0530

    update jobs page

--- src/Components/jobs/Jobs.jsx
+++ src/Components/jobs/Jobs.jsx
@@ -1,51 +1,21 @@
 import React from 'react'
 import './Job.css';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
-import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
+
 import Footer from '../Website/Footer';
 import Navbar from '../Website/Navbar';
-import {useNavigate} from 'react-router-dom'
+import { JobsItem } from './JobsItem';
+
 
 export const Jobs = () => {
-  const navigate =useNavigate()
+
   return (
     <>
-    <Navbar/>
-    <div className='jobs'>
-        <h2>Recommended Jobs for you</h2>
-        <div className='jobs-container'>
-        <div className='jobs-box'>
-          <p>Digital Manager</p>
-          <p>Amosta Solution</p>
-          <p>&#8377; 35000-45000 Per Month</p>
-          <div className='jobs-button'>
-            <div>
-              <button className='button1'><LocationOnIcon style={{fontSize:'18px'}}/> Haryana</button>
-            </div>
-            <div>
-              <button className='button2'><LocalLibraryIcon style={{fontSize:'18px'}}/> B.Com</button>
-            </div>
-            <div>
-              <button className='button3'><WorkHistoryIcon style={{fontSize:'18px'}}/> 5-7 Year</button>
-            </div>
-
-          </div>
-          <div className='jobs-button1'>
-          <div>
-            <button className='button4' onClick={()=>navigate('/jobdetails')}>Apply</button>
-          </div>
-          <div>
-            <button className='button5'>Details</button>
-          </div>
-          </div>
-        </div>
-        <div className='jobs-box'></div>
-        <div className='jobs-box'></div>
-        <div className='jobs-box'></div>
-        </div>
-      </div>
-      <Footer/>
-      </>
+      <Navbar />
+      <JobsItem
+        companyName="amosta"
+        designation="React js"
+      />
+      <Footer />
+    </>
   )
 }
---
+++ src/Components/jobs/JobsItem.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
+import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
+import {useNavigate} from 'react-router-dom'
+export const JobsItem = (props) => {
+    console.log(props,"props")
+    const navigate =useNavigate()
+  return (
+    <div className='jobs'>
+    <h2>Recommended Jobs for you</h2>
+    <div className='jobs-container'>
+    <div className='jobs-box'>
+      <p>{props.companyName}</p>
+      <p>{props.designation}</p>
+      <p>&#8377; 35000-45000 Per Month</p>
+      <div className='jobs-button'>
+        <div>
+          <button className='button1'><LocationOnIcon style={{fontSize:'18px'}}/> Haryana</button>
+        </div>
+        <div>
+          <button className='button1'><LocalLibraryIcon style={{fontSize:'18px'}}/> B.Com</button>
+        </div>
+        <div>
+          <button className='button1'><WorkHistoryIcon style={{fontSize:'18px'}}/> 5-7 Year</button>
+        </div>
+
+      </div>
+      <div className='jobs-button1'>
+      <div>
+        <button className='button4' onClick={()=>navigate('/jobdetails')}>Apply</button>
+      </div>
+      <div>
+        <button className='button5'>Details</button>
+      </div>
+      </div>
+    </div>
+    <div className='jobs-box'></div>
+    <div className='jobs-box'></div>
+    <div className='jobs-box'></div>
+    </div>
+  </div>
+  )
+}
